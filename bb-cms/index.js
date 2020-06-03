c3c58c9886038c6c340a0af4a0b1ab5f7f94ca1f
khobar <maskhobar@gmail.com>
2020-06-03 21:34:23 +0700

    cms penggalang dana

--- bb-cms/index.js
+++ bb-cms/index.js
@@ -1,11 +1,13 @@
-const { Keystone } = require('@keystonejs/keystone');
-const { GraphQLApp } = require('@keystonejs/app-graphql');
-const { AdminUIApp } = require('@keystonejs/app-admin-ui');
-const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
-
-const PROJECT_NAME = 'bantu-belajar';
-const adapterConfig = { mongoUri: 'mongodb+srv://waKwaw97:waKwaw97@cluster0-dnxx2.gcp.mongodb.net/bbcms?retryWrites=true&w=majority' };
-
+const { Keystone } = require("@keystonejs/keystone");
+const { GraphQLApp } = require("@keystonejs/app-graphql");
+const { AdminUIApp } = require("@keystonejs/app-admin-ui");
+const { MongooseAdapter: Adapter } = require("@keystonejs/adapter-mongoose");
+const PenggalangSchema = require("./lists/Penggalang");
+const PROJECT_NAME = "bantu-belajar";
+const adapterConfig = {
+  mongoUri:
+    "mongodb+srv://waKwaw97:waKwaw97@cluster0-dnxx2.gcp.mongodb.net/bbcms?retryWrites=true&w=majority",
+};
 
 /**
  * You've got a new KeystoneJS Project! Things you might want to do next:
@@ -17,6 +19,8 @@ const keystone = new Keystone({
   name: PROJECT_NAME,
   adapter: new Adapter(adapterConfig),
 });
+
+keystone.createList("Penggalang", PenggalangSchema);
 
 module.exports = {
   keystone,
---
+++ bb-cms/lists/Penggalang.js
@@ -0,0 +1,34 @@
+const { Text, DateTime, File } = require("@keystonejs/fields/");
+const { LocalFileAdapter } = require("@keystonejs/file-adapters");
+
+const fileAdapter = new LocalFileAdapter({
+  src: "./files",
+  path: "/files",
+});
+
+module.exports = {
+  fields: {
+    nama: { type: Text, isRequired: true },
+    lokasi: { type: Text, isRequired: true },
+    tgl_gabung: { type: DateTime, isRequired: true },
+    deskripsi: { type: Text, isRequired: true },
+    logo: {
+      type: File,
+      adapter: fileAdapter,
+      hooks: {
+        beforeChange: async ({ existingItem }) => {
+          if (existingItem && existingItem.file) {
+            await fileAdapter.delete(existingItem.file);
+          }
+        },
+      },
+    },
+  },
+  hooks: {
+    afterDelete: async ({ existingItem }) => {
+      if (existingItem.file) {
+        await fileAdapter.delete(existingItem.file);
+      }
+    },
+  },
+};
