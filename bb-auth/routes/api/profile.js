b84bcf8581042c1c360c3798747edb517f8fa1df
khobar <maskhobar@gmail.com>
2020-06-11 22:43:42 +0700

    perbaikan user profile

--- bb-auth/routes/api/profile.js
+++ bb-auth/routes/api/profile.js
@@ -19,14 +19,6 @@ const storage = multer.diskStorage({
     cb(null, file.originalname);
   },
 });
-// let cloudinaryStorage = multerCloudinary({ cloudinary: cloudinary });
-// let cloudinaryUpload = multer({ storage: cloudinaryStorage });
-// router.put(
-//   "/gambar",
-//   cloudinaryUpload.fields([{ name: "photo", maxCount: 1 }])
-// );
-
-// app.use(fileUpload());
 
 //@route POST api/profile/
 //@desc ngirim data profile user
@@ -73,130 +65,44 @@ router.post("/upload", (req, res, next) => {
     );
   });
 });
-//@route PUT api/profile/updatephoto
-//@desc update photo profile ke db
-router.put("/updatephoto", (req, res) => {
-  if (req.body.photo == null)
-    res.status(200).json({ message: "masukkan url foto" });
-  User.findOne({
-    email: req.body.email,
-  }).then((user) => {
-    if (user) {
-      user.photo = req.body.photo;
-      user.save();
-      res.status(200).json({ message: "foto profil diupdate di db" });
-    } else {
-      return res.status(404).json({
-        userNotFound: "tidak ada user pada database untuk diupdate",
-      });
-    }
-  });
-});
+
 //@route PUT api/profile/firstname
 //@desc update nama depan
-router.put("/firstname", (req, res) => {
-  if (req.body.firstname == null)
-    res.status(200).json({ message: "harap isikan nama depan" });
+router.put("/updateprofile", (req, res) => {
   User.findOne({
     email: req.body.email,
   }).then((user) => {
     if (user) {
-      user.firstname = req.body.firstname;
+      let newemail = req.body.newemail;
+      let firstname = req.body.firstname;
+      let lastname = req.body.lastname;
+      let gender = req.body.gender;
+      let bio = req.body.bio;
+      let dob = req.body.dob;
+      let photo = req.body.photo;
+      if (newemail) {
+        user.email = newemail;
+      }
+      if (firstname) {
+        user.firstname = firstname;
+      }
+      if (lastname) {
+        user.lastname = lastname;
+      }
+      if (bio) {
+        user.bio = bio;
+      }
+      if (gender) {
+        user.gender = gender;
+      }
+      if (dob) {
+        user.dob = dob;
+      }
+      if (photo) {
+        user.photo = photo;
+      }
       user.save();
-      res.status(200).json({ message: "firstname diupdate" });
-    } else {
-      return res.status(404).json({
-        userNotFound: "tidak ada user pada database untuk diupdate",
-      });
-    }
-  });
-});
-//@route PUT api/profile/lastname
-//@desc update nama belakang
-router.put("/lastname", (req, res) => {
-  if (req.body.lastname == null)
-    res.status(200).json({ message: "harap isikan nama belakang" });
-  User.findOne({
-    email: req.body.email,
-  }).then((user) => {
-    if (user) {
-      user.lastname = req.body.lastname;
-      user.save();
-      res.status(200).json({ message: "lastname diupdate" });
-    } else {
-      return res.status(404).json({
-        userNotFound: "tidak ada user pada database untuk diupdate",
-      });
-    }
-  });
-});
-//@route PUT api/profile/email
-//@desc update email
-router.put("/email", (req, res) => {
-  User.findOne({
-    email: req.body.email,
-  }).then((user) => {
-    if (user) {
-      user.email = req.body.newemail;
-      user.save();
-      res.status(200).json({ message: "email diupdate" });
-    } else {
-      return res.status(404).json({
-        userNotFound: "tidak ada user pada database untuk diupdate",
-      });
-    }
-  });
-});
-//@route PUT api/profile/gender
-//@desc update gender
-router.put("/gender", (req, res) => {
-  if (req.body.gender == null)
-    res.status(200).json({ message: "harap isikan jenis kelamin" });
-  User.findOne({
-    email: req.body.email,
-  }).then((user) => {
-    if (user) {
-      user.gender = req.body.gender;
-      user.save();
-      res.status(200).json({ message: "gender diupdate" });
-    } else {
-      return res.status(404).json({
-        userNotFound: "tidak ada user pada database untuk diupdate",
-      });
-    }
-  });
-});
-//@route PUT api/profile/dob
-//@desc update tanggal lahir
-router.put("/dob", (req, res) => {
-  if (req.body.dob == null)
-    res.status(200).json({ message: "harap isikan tanggal lahir" });
-  User.findOne({
-    email: req.body.email,
-  }).then((user) => {
-    if (user) {
-      user.dob = req.body.dob;
-      user.save();
-      res.status(200).json({ message: "date of birth diupdate" });
-    } else {
-      return res.status(404).json({
-        userNotFound: "tidak ada user pada database untuk diupdate",
-      });
-    }
-  });
-});
-//@route PUT api/profile/bio
-//@desc update biografi
-router.put("/bio", (req, res) => {
-  if (req.body.bio == null)
-    res.status(200).json({ message: "harap isikan bio" });
-  User.findOne({
-    email: req.body.email,
-  }).then((user) => {
-    if (user) {
-      user.bio = req.body.bio;
-      user.save();
-      res.status(200).json({ message: "biografi diupdate" });
+      res.status(200).json({ message: "data user selesai diupdate" });
     } else {
       return res.status(404).json({
         userNotFound: "tidak ada user pada database untuk diupdate",
